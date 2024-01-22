b162f39a8e1ea64e07d0b627c4034a8029debce0
GioacchinoArgo <gioacchinogabri.argo@libero.it>
2024-01-22 21:28:08 +0100

    added methods for buttons

--- js/script.js
+++ js/script.js
@@ -10,7 +10,18 @@ const app= createApp({
             currentIndex: 0
         }
     },
-
+    methods: {
+        goToThePrev() {
+            const firstIndex = this.destinations.length -1;
+            if(this.currentIndex === 0) this.currentIndex = firstIndex;
+            else this.currentIndex--;
+        },
+        goToTheNext(){
+            const lastIndex = this.destinations.length -1;
+            if(this.currentIndex === lastIndex) this.currentIndex = 0;
+            else this.currentIndex++;
+        }
+    }
 });
 
 app.mount('#root'); 
